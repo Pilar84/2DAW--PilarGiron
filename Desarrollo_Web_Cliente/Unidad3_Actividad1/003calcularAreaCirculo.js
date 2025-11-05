7b384a4c329e81e2478d2fdd316aa7aec42a6c10
Pilar84 <pilargijor@hotmail.es>
2025-11-05 13:32:02 +0100

    "Realizado ej 3 y 4 y subido al remoto"

---
+++ Desarrollo_Web_Cliente/Unidad3_Actividad1/003calcularAreaCirculo.js
@@ -0,0 +1,6 @@
+function calcularAreaCirculo(radio) {
+    areaCirculo= Math.PI * Math.pow(radio, 2);
+    return areaCirculo;
+}
+console.log("El área del círculo es: " + calcularAreaCirculo(5));
+
