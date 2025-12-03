bc34d4b0100ef6171d2e173e7b4a23506b9d5fad
Pilar84 <pilargijor@hotmail.es>
2025-12-03 21:41:25 +0100

    Implement initial setup for the Flora project, including models, views, and templates for artists, editions, installations, and venues. Added migration files and created HTML templates for artist creation, detail, and editing, along with placeholders for editions and installations.

--- Desarrollo_Web_Servidor/Proyecto_Final/flora/views.py
+++ Desarrollo_Web_Servidor/Proyecto_Final/flora/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Artist, Installation, Venue, Edition
+from django.contrib import messages
 
 
 
@@ -8,27 +9,38 @@ from .models import Artist, Installation, Venue, Edition
 
 # Create your views here.
 
-#creación
+# Creación de artista
 def artist_create(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         country = request.POST.get('country')
         short_bio = request.POST.get('short_bio')
         website = request.POST.get('website')
-        #crear objeto
-        artist = Artist(name=name, country=country, short_bio=short_bio, website=website)
+
+        # Crear objeto Artist
+        artist = Artist(
+            name=name,
+            country=country,
+            short_bio=short_bio,
+            website=website
+        )
         artist.save()
-        
-        #esto lo que permite es que si recargo la página, se vuelve a enviar el POST y se crea el artista otra vez.
-        return redirect(request, 'artist_create.html', {'artist': artist})
+
+        messages.success(request, 'Artista creado correctamente.')
+        return redirect('artist_list')
     else:
         return render(request, 'artist_create.html')
 
 
+
 # Artist: Listado de artistas
 def artist_list(request):
-    artists = Artist.objects.all()
-    return render(request, 'artist_list.html', {'artists': artists})
+    country = request.GET.get('country', '').strip()
+    if country:
+        artists = Artist.objects.filter(country__icontains=country)
+    else:
+        artists = Artist.objects.all()
+    return render(request, 'artist_list.html', {'artists': artists, 'country': country})
 
 #detalle incluyendo obras
 def artist_detail(request, artist_id):
@@ -44,7 +56,11 @@ def artist_edit(request, artist_id):
         artist.short_bio = request.POST.get('short_bio')
         artist.website = request.POST.get('website')
         artist.save()
-        return redirect('artist_detail', artist_id=artist.id)
+        # Mensaje de éxito
+        messages.success(request, 'Artista editado correctamente.')
+
+        # Redirigir al listado de artistas
+        return redirect('artist_list')
     else:
         return render(request, 'artist_edit.html', {'artist': artist})
     
