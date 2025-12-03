ce079103acd1674f594044b1f553cd36d9ae6695
Pilar84 <pilargijor@alumnos.ilerna.com>
2025-12-03 13:28:43 +0100

    Add URL routing and views for managing artists, installations, venues, and editions; create corresponding HTML templates

--- Desarrollo_Web_Servidor/Proyecto_Final/config/urls.py
+++ Desarrollo_Web_Servidor/Proyecto_Final/config/urls.py
@@ -16,8 +16,36 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from flora import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    pa
+    # ARTIST
+    path('artists/', views.artist_list, name='artist_list'),
+    path('artists/create/', views.artist_create, name='artist_create'),
+    path('artists/<int:artist_id>/', views.artist_detail, name='artist_detail'),
+    path('artists/<int:artist_id>/edit/', views.artist_edit, name='artist_edit'),
+    path('artists/<int:artist_id>/delete/', views.artist_delete, name='artist_delete'),
+    path('artists/country/<str:country>/', views.artist_list_by_country, name='artist_list_by_country'),
+
+    # INSTALLATION
+    path('installations/', views.installation_list, name='installation_list'),
+    path('installations/create/', views.installation_create, name='installation_create'),
+    path('installations/<int:installation_id>/', views.installation_detail, name='installation_detail'),
+    path('installations/edition/<int:edition_id>/', views.installation_list_by_edition, name='installation_list_by_edition'),
+    path('installations/order/<str:order>/', views.installation_list_by_opening_date, name='installation_list_by_opening_date'),
+
+    # VENUE
+    path('venues/', views.venue_list, name='venue_list'),
+    path('venues/create/', views.venue_create, name='venue_create'),
+    path('venues/<int:venue_id>/', views.venue_detail, name='venue_detail'),
+
+    # EDITION
+    path('editions/', views.edition_list, name='edition_list'),
+    path('editions/create/', views.edition_create, name='edition_create'),
+    path('editions/<int:edition_id>/', views.edition_detail, name='edition_detail'),
+    
+    #HOME
+    path('', views.home, name='home'),
 ]
--- Desarrollo_Web_Servidor/Proyecto_Final/flora/views.py
+++ Desarrollo_Web_Servidor/Proyecto_Final/flora/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import Artist  
+from .models import Artist, Installation, Venue, Edition
+
+
 
 
 
@@ -45,3 +47,117 @@ def artist_edit(request, artist_id):
         return redirect('artist_detail', artist_id=artist.id)
     else:
         return render(request, 'artist_edit.html', {'artist': artist})
+    
+#borrado
+def artist_delete(request, artist_id):
+    artist = Artist.objects.get(id=artist_id)
+    artist.delete()
+    return redirect('artist_list')
+
+#filtrado por pais
+def artist_list_by_country(request, country):
+    artists = Artist.objects.filter(country=country)
+    return render(request, 'artist_list.html', {'artists': artists})
+
+#INSTALLATION
+
+#listado
+def installation_list(request):
+    installations = Installation.objects.all()
+    return render(request, 'installation_list.html', {'installations': installations})
+
+#detalle incluyendo autor
+def installation_detail(request, installation_id):
+    installation = Installation.objects.get(id=installation_id)
+    return render(request, 'installation_detail.html', {'installation': installation})
+
+#creacion
+def installation_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        opening_date = request.POST.get('opening_date')
+        short_description = request.POST.get('short_description')
+        materials = request.POST.get('materials')
+        artist_id = request.POST.get('artist')
+        venue_id = request.POST.get('venue')
+        edition_id = request.POST.get('edition')
+        artist = Artist.objects.get(id=artist_id)
+        venue = Venue.objects.get(id=venue_id)
+        edition = Edition.objects.get(id=edition_id)
+        installation = Installation(title=title, opening_date=opening_date, short_description=short_description, materials=materials, artist=artist, venue=venue, edition=edition)
+        installation.save()
+        return redirect('installation_detail', installation_id=installation.id)
+    else:
+        artists = Artist.objects.all()
+        venues = Venue.objects.all()
+        editions = Edition.objects.all()
+        return render(request, 'installation_create.html', {'artists': artists, 'venues': venues, 'editions': editions})    
+    
+#filtrado por edicion
+def installation_list_by_edition(request, edition_id):
+    installations = Installation.objects.filter(edition_id=edition_id)
+    return render(request, 'installation_list.html', {'installations': installations})
+
+
+#Ordenar por fecha de inauguración (de forma ascendente o descendente).
+def installation_list_by_opening_date(request, order):
+    installations = Installation.objects.order_by('opening_date' if order == 'asc' else '-opening_date')
+    return render(request, 'installation_list.html', {'installations': installations})
+
+#VENUE
+
+#listado
+def venue_list(request):
+    venues = Venue.objects.all()
+    return render(request, 'venue_list.html', {'venues': venues})
+
+#detalle incluyendo obras
+def venue_detail(request, venue_id):
+    venue = Venue.objects.get(id=venue_id)
+    return render(request, 'venue_detail.html', {'venue': venue})
+
+#creacion
+def venue_create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+        description = request.POST.get('description')
+        max_capacity = request.POST.get('max_capacity')
+        venue = Venue(name=name, address=address, description=description, max_capacity=max_capacity)
+        venue.save()
+        return redirect('venue_detail', venue_id=venue.id)
+    else:
+        return render(request, 'venue_create.html')
+    
+#EDICCIÓN
+
+#listado
+def edition_list(request):
+    editions = Edition.objects.all()
+    return render(request, 'edition_list.html', {'editions': editions})
+
+#detalle incluyendo obras
+def edition_detail(request, edition_id):
+    edition = Edition.objects.get(id=edition_id)
+    return render(request, 'edition_detail.html', {'edition': edition})
+
+#creacion
+def edition_create(request):
+    if request.method == 'POST':
+        year = request.POST.get('year')
+        theme = request.POST.get('theme')
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+        edition = Edition(year=year, theme=theme, start_date=start_date, end_date=end_date)
+        edition.save()
+        return redirect('edition_detail', edition_id=edition.id)
+    else:
+        return render(request, 'edition_create.html')
+    
+    
+#PAGINA DE INICIO
+
+def home(request):
+    return render(request, 'index.html')
+
+
