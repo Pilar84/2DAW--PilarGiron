d8f4e7250267b7a4dacd031790c3d2de358aaef2
Pilar84 <pilargijor@alumnos.ilerna.com>
2026-04-06 13:57:24 +0200

    creacion de la vista library_entry y  url correspondiente

--- Desarrollo_Web_Servidor/Steamlike/library/views.py
+++ Desarrollo_Web_Servidor/Steamlike/library/views.py
@@ -1,6 +1,15 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
+from .models import LibraryEntry
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 @require_GET
 def health(request):
     return JsonResponse({"status": "ok"})
+
+# aqui vamos a crear la vista POST
+@require_POST
+def add_library_entry(request): 
+    return JsonResponse({"message": "Endpoint funcionando correctamente"})
+
--- Desarrollo_Web_Servidor/Steamlike/steamlike_backend/urls.py
+++ Desarrollo_Web_Servidor/Steamlike/steamlike_backend/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from library.views import health
+from library.views import add_library_entry 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     #path("api/library/", include("core.urls")),
-    path("api/health/", health)
-]
+    path("api/health/", health),
+    path("api/library/add/", add_library_entry),
+]   
+
+
