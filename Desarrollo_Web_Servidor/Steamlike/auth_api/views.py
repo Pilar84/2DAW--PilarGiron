a539915e7fc7f30936b515ad9778765426def4c0
Pilar84 <pilargijor@alumnos.ilerna.com>
2026-04-27 11:44:33 +0200

    refactor: Remove unused test file and clean up imports in views.py

--- Desarrollo_Web_Servidor/Steamlike/auth_api/views.py
+++ Desarrollo_Web_Servidor/Steamlike/auth_api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.test import TestCase
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.contrib.auth.models import User
