3584c3849d5aa09720066dd3fff2449ac3375288
Pilar84 <pilargijor@hotmail.es>
2026-07-15 11:04:04 +0200

    Configura WhiteNoise y STATIC_ROOT

--- Desarrollo_Web_Servidor/Steamlike/steamlike_backend/settings.py
+++ Desarrollo_Web_Servidor/Steamlike/steamlike_backend/settings.py
@@ -45,9 +45,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
 
-    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -120,6 +121,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # --- CORS + cookies (SessionAuthentication) ---
